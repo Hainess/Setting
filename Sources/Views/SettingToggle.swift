4b78a7a20c693e5515bde4ee5611e16a0c230770
whh <whh@meitu.com>
2025-10-04 02:03:30 +0800

    [feat] SettingToggle - Support Icon

--- Sources/Views/SettingToggle.swift
+++ Sources/Views/SettingToggle.swift
@@ -15,33 +15,41 @@ public struct SettingToggle: View, Setting {
     public var id: AnyHashable?
     public var title: String
     @Binding public var isOn: Bool
+    public var icon: SettingIcon?
     public var horizontalSpacing = CGFloat(12)
     public var verticalPadding = CGFloat(14)
     public var horizontalPadding: CGFloat? = nil
+    public var onChange: ((Bool) -> Void)? // Add onChange closure
 
     public init(
         id: AnyHashable? = nil,
+        icon: SettingIcon? = nil,
         title: String,
         isOn: Binding<Bool>,
         horizontalSpacing: CGFloat = CGFloat(12),
         verticalPadding: CGFloat = CGFloat(14),
-        horizontalPadding: CGFloat? = nil
+        horizontalPadding: CGFloat? = nil,
+        onChange: ((Bool) -> Void)? = nil
     ) {
         self.id = id
+        self.icon = icon
         self.title = title
         self._isOn = isOn
         self.horizontalSpacing = horizontalSpacing
         self.verticalPadding = verticalPadding
         self.horizontalPadding = horizontalPadding
+        self.onChange = onChange
     }
 
     public var body: some View {
         SettingToggleView(
+            icon: icon,
             title: title,
             isOn: $isOn,
             horizontalSpacing: horizontalSpacing,
             verticalPadding: verticalPadding,
-            horizontalPadding: horizontalPadding
+            horizontalPadding: horizontalPadding,
+            onChange: onChange
         )
     }
 }
@@ -49,15 +57,21 @@ public struct SettingToggle: View, Setting {
 struct SettingToggleView: View {
     @Environment(\.edgePadding) var edgePadding
     
+    let icon: SettingIcon?
     let title: String
     @Binding var isOn: Bool
 
     var horizontalSpacing = CGFloat(12)
     var verticalPadding = CGFloat(14)
     var horizontalPadding: CGFloat? = nil
+    var onChange: ((Bool) -> Void)?
 
     var body: some View {
         HStack(spacing: horizontalSpacing) {
+            if let icon {
+                SettingIconView(icon: icon)
+            }
+            
             Text(title)
                 .fixedSize(horizontal: false, vertical: true)
                 .frame(maxWidth: .infinity, alignment: .leading)
@@ -65,8 +79,31 @@ struct SettingToggleView: View {
 
             Toggle("", isOn: $isOn)
                 .labelsHidden()
+                .onChange(of: isOn, perform: { newValue in
+                    onChange?(newValue)
+                })
         }
         .padding(.horizontal, horizontalPadding ?? edgePadding)
         .accessibilityElement(children: .combine)
+    }
+}
+
+public extension SettingToggle {
+    func icon(_ icon: String, color: Color = .blue) -> SettingToggle {
+        var toggle = self
+        toggle.icon = .system(icon: icon, backgroundColor: color)
+        return toggle
+    }
+
+    func icon(_ icon: String, foregroundColor: Color = .white, backgroundColor: Color = .blue) -> SettingToggle {
+        var toggle = self
+        toggle.icon = .system(icon: icon, foregroundColor: foregroundColor, backgroundColor: backgroundColor)
+        return toggle
+    }
+
+    func icon(icon: SettingIcon) -> SettingToggle {
+        var toggle = self
+        toggle.icon = icon
+        return toggle
     }
 }
